857f882cab3b9d052604d85d3f664cda9916266f
Stouse49 <Stouse49@gmail.com>
2019-07-23 17:44:17 -0700

    Set default precision to 0.0000 GLC from 0.00 mGLC

--- wallet/src/de/schildbach/wallet/Configuration.java
+++ wallet/src/de/schildbach/wallet/Configuration.java
@@ -67,8 +67,8 @@ public class Configuration {
     private static final String PREFS_KEY_LAST_BACKUP = "last_backup";
     private static final String PREFS_KEY_LAST_BLUETOOTH_ADDRESS = "last_bluetooth_address";
 
-    private static final int PREFS_DEFAULT_BTC_SHIFT = 3;
-    private static final int PREFS_DEFAULT_BTC_PRECISION = 2;
+    private static final int PREFS_DEFAULT_BTC_SHIFT = 0;
+    private static final int PREFS_DEFAULT_BTC_PRECISION = 4;
 
     private static final Logger log = LoggerFactory.getLogger(Configuration.class);
 
